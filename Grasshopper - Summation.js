3f7588fe0180917442576935fffbd987e1f9db83
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-01-21 21:46:18 +0200

    solve Grasshopper - Summation

---
+++ Grasshopper - Summation.js
@@ -0,0 +1,29 @@
+// Summation
+// Write a program that finds the summation of every number from 1 to num.
+//  The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+///////////// Solution //////////////
+
+var summation = function (num) {
+    // Code here
+    var resulte = 0; 
+    for(let i = 0; i <= num; i++){
+      resulte += i;
+    }
+    return resulte;
+  }
+
+
+
+
+
+
+
