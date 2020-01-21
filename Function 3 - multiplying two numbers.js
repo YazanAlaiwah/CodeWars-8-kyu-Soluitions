8c191c5d00964fb939242f95d6bc1f4eea5ff14b
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-01-22 01:03:07 +0200

    solve Function 3 - multiplying two numbers

---
+++ Function 3 - multiplying two numbers.js
@@ -0,0 +1,12 @@
+// This function has to be called multiply and needs to take two numbers as arguments,
+//  and has to return the multiplication of the two arguments.
+
+///////////// Solution //////////////
+
+// Write here your multiply-function
+function multiply(a , b){
+    return a * b
+   }
+
+
+
