634d5dd38771169ff437bfb1eda5ff9fed0ed93b
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-01-21 20:19:30 +0200

    solve Even or Odd

---
+++ Even or Odd.js
@@ -0,0 +1,13 @@
+// Create a function (or write a script in Shell) that takes an integer as 
+// an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+///////////// Solution //////////////
+
+function even_or_odd(number) {
+    // ...
+    return number % 2 === 0 ? "Even" : "Odd";
+  }
+
+
+
