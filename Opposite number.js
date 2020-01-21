deb4d675e38f9d46e8163b246fd676dd46b30637
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-01-21 20:21:31 +0200

    solve Opposite number

---
+++ Opposite number.js
@@ -0,0 +1,17 @@
+
+// Very simple, given a number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+///////////// Solution //////////////
+
+function opposite(number) {
+    //your code here
+    return number * -1
+  }
+
+
