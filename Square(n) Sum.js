0cb8c1e9129861a2bdcc55fa20bdfcbb35a2bff3
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-01-21 22:08:10 +0200

    solve Square(n) Sum

---
+++ Square(n) Sum.js
@@ -0,0 +1,16 @@
+// Complete the square sum function so that it squares each number passed 
+// into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+///////////// Solution //////////////
+
+function squareSum(numbers){
+    return numbers.reduce((a,c)=> a + c * c,0)
+  }
+
+
+
+
+
+
