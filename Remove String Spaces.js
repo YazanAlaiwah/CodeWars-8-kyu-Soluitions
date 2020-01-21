9eac4fc642556af83d0a02ee458e1264d2df4e8e
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-01-21 21:22:35 +0200

    solve Remove String Spaces

---
+++ Remove String Spaces.js
@@ -0,0 +1,14 @@
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+
+///////////// Solution //////////////
+
+function noSpace(x){
+    return x.split(" ").join("")
+   }
+
+
+
+
+
