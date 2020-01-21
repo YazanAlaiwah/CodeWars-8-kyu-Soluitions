bd2e405ea5ee51f212dd4985e514a6ce93ae295c
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-01-22 01:37:17 +0200

    solve Rock Paper Scissors!

---
+++ Rock Paper Scissors!.js
@@ -0,0 +1,29 @@
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples:
+
+// rps('scissors','paper') // Player 1 won!
+// rps('scissors','rock') // Player 2 won!
+// rps('paper','paper') // Draw!
+
+///////////// Solution //////////////
+
+const rps = (p1, p2) => {
+
+    if(p1 === p2){
+      return "Draw!"
+    }else if(p1 === "scissors" && p2 === 'paper'){
+      return 'Player 1 won!'
+    }else if(p1 === 'paper' && p2 === 'rock'){
+      return 'Player 1 won!'
+    }else if(p1 === 'rock' && p2 === 'scissors'){
+      return 'Player 1 won!'
+    }else{
+    return "Player 2 won!"
+    }
+    };
+
+
+
+
