34697ed3987bd8c50e2fb1789a8b1d3af0227148
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-01-21 21:29:39 +0200

    solve Convert a Number to a String!

---
+++ Convert a Number to a String!.js
@@ -0,0 +1,20 @@
+
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// numberToString(123); // returns '123';`   
+// numberToString(999); // returns '999';`
+
+///////////// Solution //////////////
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString()
+ }
+
+
+
+
+
