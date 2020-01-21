3052d6dfe9434dd3ed352e25b63e818c78fcfe34
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-01-21 20:48:40 +0200

    solve String repeat

---
+++ String repeat.js
@@ -0,0 +1,15 @@
+// Write a function called repeatString which repeats the given String src exactly count times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+///////////// Solution //////////////
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+
+
+
+
+
