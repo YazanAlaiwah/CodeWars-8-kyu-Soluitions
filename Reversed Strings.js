f13f1038f562a1a2e149b1bb937f463c5fb0a724
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-01-21 21:48:57 +0200

    solve Reversed Strings

---
+++ Reversed Strings.js
@@ -0,0 +1,14 @@
+// Complete the solution so that it reverses the string value passed into it.
+
+// solution('world'); // returns 'dlrow'
+
+///////////// Solution //////////////
+
+function solution(str){
+    return str.split("").reverse().join("")
+  }
+
+
+
+
+
