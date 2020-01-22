177f89de4b0c2460781dcce623cacad8db6cd10f
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-01-22 17:51:06 +0200

    solve Basic variable assignment

---
+++ Basic variable assignment.js
@@ -0,0 +1,12 @@
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+
+
+
+///////////// Solution //////////////
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+
+
