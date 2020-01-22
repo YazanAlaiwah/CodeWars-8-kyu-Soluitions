2a7325db001c6ff818259a81b782b128bedfab15
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-01-22 17:59:33 +0200

    solve Switch it Up!

---
+++ Switch it Up!.js
@@ -0,0 +1,29 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+///////////// Solution //////////////
+
+function switchItUp(number){
+    //Write your own Code!
+      var obj = {
+        0 : 'Zero',
+        1 : 'One',
+        2 : 'Two',
+        3 : 'Three',
+        4 : 'Four',
+        5 : 'Five',
+        6 : 'Six',
+        7 : 'Seven',
+        8 : 'Eight',
+        9 : 'Nine'
+      } 
+      return obj[number]
+    }
+
+
+
